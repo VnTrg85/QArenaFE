a4652e42dc492610828882b7d46e8b631571019a
VnTrg85 <truongddh85@gmail.com>
2025-04-28 23:04:14 +0700

    test rebase

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -11,6 +11,7 @@ const cx = classname.bind(styles);
 const Login = () => {
 	const navigate = useNavigate();
 	const { showToast } = useToast();
+	console.log(1);
 	//State
 	const { setUserValue, getUserValue } = useUser();
 	const [data, setData] = useState({
